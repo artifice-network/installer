f81d2141e5c0e9224456a128320b0283f106e9f5
Julian Lazaras <umbrasdracodominus@gmail.com>
2020-07-21 02:38:12 -0700

    attempting to fix dep issue

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,9 @@ use std::time::Duration;
 use manager::database::ArtificePeers;
 use std::io::{Read, Write};
 use networking::ArtificeConfig;
+use std::fs::{create_dir, File};
 
-fn get_password() -> std::io::Result<String>{
+pub fn get_password() -> std::io::Result<String>{
     let mut first_key: [u8;65535] = [0;65535];
     let mut second_key: [u8;65535] = [0;65535];
     std::io::stdout().lock().write(b"enter encryption key: ")?;
@@ -32,9 +33,18 @@ fn main() {
     let password = get_password().unwrap();
     let mut installer = Installer::new(InstallationSrc::NewCompiled, database, 4, Duration::from_secs(5000000));
     let first_task = Task::<std::io::Error, ArtificeDB>::new(1, "create", move |database, _completed_tasks|{
-        // kept unused until complete install possible
-        let _peers: ArtificePeers = database.create_table("peers".to_string(), &password.clone().into_bytes())?;
-        let _config: ArtificeConfig = database.load_entry("config".to_string(), &password.clone().into_bytes())?;
+        let root = format!("{}/.artifice/", dirs::home_dir().unwrap().display());
+        create_dir(&root)?;
+        let users = format!("{}peers", &root);
+        create_dir(&users)?;
+        File::create(&format!("{}config.artusr", &root))?;
+        File::create(&format!("{}permissions.artusr", &root))?;
+        create_dir(&format!("{}applications", root))?;
+        create_dir(&format!("{}bin/", root))?;
+        Ok(())
+    });
+    let second_task = Task::<std::io::Error, ArtificeDB>::new(2, "download", move |_, _|{
+        //let config = ArtificeConfig::generate();
         
         Ok(())
     });
